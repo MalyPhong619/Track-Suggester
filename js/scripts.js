67b3ff88e68a9f96db5f79ece7c719f4909f8b7c
Maly Phongsavanh <m.phongsavanh619@gmail.com>
2018-10-19 09:27:32 -0700

    create branch for first form on submit

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,18 @@ $(document).ready(function() {
   $("#userInputForm").submit(function(event) {
     event.preventDefault();
 
+    var userName = $("#formName").val();
+    var userChoice = $("#programChoice").val();
 
-
+    if (userChoice === "yes") {
+      $("#questionForm").show();
+      $("#userInputForm").hide()
+    } else if (userChoice === "no") {
+      $("#knowTrack").show();
+      $(".container").hide();
+    } else {
+      alert("Please make a selection");
+    }
 
   });
 });
